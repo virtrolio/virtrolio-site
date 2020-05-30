e564d0fdff48c0ad31da943a354779bccd1b6730
Janakitti <janakittiratana@gmail.com>
2020-05-30 11:54:02 -0400

    Add carousel/cards toggle

--- src/app/viewing/messages/messages.component.ts
+++ src/app/viewing/messages/messages.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  viewMode: string = 'Carousel';
   userMsgData = [{  'author': 'Rammy',
                     'message': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
                     'fontfamily': 'montserrat',
@@ -27,6 +28,10 @@ export class MessagesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleViewStyle() {
+    this.viewMode = this.viewMode === 'Carousel' ? 'Cards' : 'Carousel';
   }
 
 }
