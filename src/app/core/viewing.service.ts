1463b3b514fea50b89b463805b9f153a2e460261
Janakitti <janakittiratana@gmail.com>
2020-06-02 20:22:04 -0400

    Modify styling

---
+++ src/app/core/viewing.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ViewingService {
+  userMsgData = [{  'author': 'Rammy',
+                    'message': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                    'fontfamily': 'Montserrat',
+                    'fontstyle': 'regular',
+                    'fontcolor': '#FFFFFF',
+                    'bgcolor': '#55d3f2'},
+                  { 'author': 'Alice',
+                    'message': 'Have a good summer',
+                    'fontfamily': 'Courier New',
+                    'fontstyle': 'bold',
+                    'fontcolor': '#FFFFFF',
+                    'bgcolor': '#ff6b8b'},
+                  { 'author': 'Bob',
+                    'message': 'Goodbye',
+                    'fontfamily': 'Montserrat',
+                    'fontstyle': 'italic',
+                    'fontcolor': '#696969',
+                    'bgcolor': '#f76534'}];
+  constructor() { }
+}
--- src/app/viewing/messages/messages.component.ts
+++ src/app/viewing/messages/messages.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ViewingService } from '../../core/viewing.service';
+
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -7,25 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
   isCardView: boolean = false;
-  userMsgData = [{  'author': 'Rammy',
-                    'message': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-                    'fontfamily': 'Montserrat',
-                    'fontstyle': 'regular',
-                    'fontcolor': '#FFFFFF',
-                    'bgcolor': '#55d3f2'},
-                  { 'author': 'Alice',
-                    'message': 'Have a good summer',
-                    'fontfamily': 'Courier New',
-                    'fontstyle': 'bold',
-                    'fontcolor': '#FFFFFF',
-                    'bgcolor': '#ff6b8b'},
-                  { 'author': 'Bob',
-                    'message': 'Goodbye',
-                    'fontfamily': 'Montserrat',
-                    'fontstyle': 'italic',
-                    'fontcolor': '#696969',
-                    'bgcolor': '#f76534'}];
-  constructor() { }
+  userMsgData;
+  constructor(private viewingService: ViewingService) { 
+    this.userMsgData = viewingService.userMsgData;
+  }
 
   ngOnInit(): void {
   }
