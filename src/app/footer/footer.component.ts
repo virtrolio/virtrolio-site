5f61d1294e2110e8d05855c19246f176494c93fb
GuyInFridge <dabdemonic@gmail.com>
2020-06-06 09:47:38 -0400

    Add test footer html file

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
+  templateUrl: './test.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
