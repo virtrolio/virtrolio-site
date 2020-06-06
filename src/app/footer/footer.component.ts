b31f07604a6a9dfc55c838e9c5eb7d92696e82ce
GuyInFridge <dabdemonic@gmail.com>
2020-06-06 09:55:10 -0400

    Create custom footer and vertically center icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './test.html',
+  templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
