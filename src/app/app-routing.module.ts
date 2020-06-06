c9195e5c2136f9183efae26fc9f44f6334736ae2
Tommy Huang <tommyhuang999@gmail.com>
2020-06-06 15:00:39 -0400

    Fix app-routing-module to use dashes (wasn't getting pulled for some reason)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SigningComponent } from './signing/signing.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'pagenotfound', component: PagenotfoundComponent },
+  { path: 'pagenotfound', component: PageNotFoundComponent },
   { path: 'signing', component: SigningComponent },
   { path: '**', pathMatch: 'full', redirectTo: '/pagenotfound'}
 ];
