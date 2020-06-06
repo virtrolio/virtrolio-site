75978c63513e297b7c4ab87485fb7d384f149d4f
Eric C <39015447+ricetech@users.noreply.github.com>
2020-06-06 15:13:32 -0400

    Fix merge conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SettingsComponent } from "./settings/settings.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: 'settings', component: SettingsComponent},
   { path: '**', pathMatch: 'full', redirectTo: '/page-not-found'}
 ];
 
