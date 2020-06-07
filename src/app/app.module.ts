bc06f69711833a3d66c0d5be9908a8f7e5061d04
Eric C <39015447+ricetech@users.noreply.github.com>
2020-06-07 14:50:20 -0400

    Import ngx-markdown and emoji-toolkit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { AngularFireAnalyticsModule } from "@angular/fire/analytics";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 
+// ngx-markdown
+import { MarkdownModule } from "ngx-markdown";
+
 // App
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +37,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     AngularFireAnalyticsModule,
     AngularFirestoreModule,
     AngularFireAuthModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
