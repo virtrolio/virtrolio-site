32c76ad616815ed68a543e673099b4e546819f5f
Eric C <39015447+ricetech@users.noreply.github.com>
2020-06-08 17:50:42 -0400

    Add isLoggedIn() and profilePictureLink()

--- src/app/core/app-auth.service.ts
+++ src/app/core/app-auth.service.ts
@@ -26,4 +26,16 @@ export class AppAuthService {
       return this._router.navigate([ "/" ])
     });
   }
+
+  isLoggedIn() {
+    return this._auth.currentUser !== null
+  }
+
+  async profilePictureLink() {
+    if (this._auth.currentUser) {
+      return (await this._auth.currentUser).photoURL
+    } else {
+      throw new TypeError("User is not logged in")
+    }
+  }
 }
