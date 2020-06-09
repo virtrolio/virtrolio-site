e1ab162ba0bbd1220d501fd617188d3f2755c6ee
Eric C <39015447+ricetech@users.noreply.github.com>
2020-06-09 00:33:03 -0400

    Add missing imports in core.module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "../app-routing.module";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-    imports: [ ],
-    declarations: [
-      NavbarComponent,
-      FooterComponent
-    ],
-    exports: [
-      NavbarComponent,
-      FooterComponent
-    ],
-    providers: [ ]
+  imports: [
+    RouterModule,
+    AppRoutingModule,
+    CommonModule
+  ],
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent
+  ],
+  providers: []
 })
-export class CoreModule { }
+export class CoreModule {
+}
