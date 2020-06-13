68f47e56ecd0c7db6c73680375843fbfa0dd9299
Tommy Huang <tommyhuang999@gmail.com>
2020-06-12 22:18:29 -0400

    Remove useless setSelectionRange

--- src/app/pages/signing/signing.component.ts
+++ src/app/pages/signing/signing.component.ts
@@ -16,43 +16,34 @@ export class SigningComponent implements OnInit {
   selectFont(font: string) {
     this.currentFont = font;
   }
-
   makeBold(textbox: HTMLInputElement) {
     const start = textbox.selectionStart;
     const end = textbox.selectionEnd;
     const text = textbox.value;
     this.signingBoxText = text.slice(0, start) + '**' + text.slice(start, end) + '**' + text.slice(end);
     textbox.select();
-    textbox.setSelectionRange(start - 2, end + 2);
   }
-
   makeItalics(textbox: HTMLInputElement) {
     const start = textbox.selectionStart;
     const end = textbox.selectionEnd;
     const text = textbox.value;
     this.signingBoxText = text.slice(0, start) + '*' + text.slice(start, end) + '*' + text.slice(end);
     textbox.select();
-    textbox.setSelectionRange(start - 1, end + 1);
   }
-
   makeUnderline(textbox: HTMLInputElement) {
     const start = textbox.selectionStart;
     const end = textbox.selectionEnd;
     const text = textbox.value;
     this.signingBoxText = text.slice(0, start) + '<u>' + text.slice(start, end) + '</u>' + text.slice(end);
     textbox.select();
-    textbox.setSelectionRange(start - 3, end + 4);
   }
-
   makeStrikethrough(textbox: HTMLInputElement) {
     const start = textbox.selectionStart;
     const end = textbox.selectionEnd;
     const text = textbox.value;
     this.signingBoxText = text.slice(0, start) + '~~' + text.slice(start, end) + '~~' + text.slice(end);
     textbox.select();
-    textbox.setSelectionRange(start - 2, end + 2);
   }
-
 
   constructor() { }
 
