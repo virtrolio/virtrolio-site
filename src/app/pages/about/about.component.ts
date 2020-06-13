c0a199b7f05580673bc31f4a53fd508cfd35347c
John Zhou <john2001@live.ca>
2020-06-13 15:54:12 -0400

    Fixed HEIC bug (just use JPG or PNG)

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { $ } from 'protractor';
+import { strict } from 'assert';
 
 @Component({
   selector: 'app-about',
@@ -8,47 +9,70 @@ import { $ } from 'protractor';
 })
 export class AboutComponent implements OnInit {
 
-
-
   constructor() { }
 
-  team_1_silly() {
-    console.log('Dabeer enter');
+  private imagePaths = [
+    './../../../assets/images/about-us-developers/dabeer.jpg',
+    './../../../assets/images/about-us-developers/dabeer-silly.png',
+    './../../../assets/images/about-us-developers/arvind.jpg',
+    './../../../assets/images/about-us-developers/arvind-silly.jpg',
+    './../../../assets/images/about-us-developers/eric.jpg',
+    './../../../assets/images/about-us-developers/eric-silly.png',
+    './../../../assets/images/about-us-developers/tommy.jpg',
+    './../../../assets/images/about-us-developers/tommy-silly.jpg'
+  ];
 
-    document.getElementById('team-image-1').setAttribute('src', './../../../assets/images/about-us-developers/dabeer-silly.png');
+  private imageIds = [
+    'team-image-0',
+    'team-image-1',
+    'team-image-2',
+    'team-image-3'
+  ];
 
+  changePhotoSilly(personNumber) {
+    console.log(this.imagePaths[personNumber * 2 + 1]);
+    document.getElementById('team-image-' + personNumber.toString()).setAttribute('src', this.imagePaths[personNumber * 2 + 1]);
   }
-
-  team_1_regular() {
-    console.log('Dabeer exit');
-    document.getElementById('team-image-1').setAttribute('src', './../../../assets/images/about-us-developers/dabeer.jpg');
-
-  }
-
-  team_2_silly() {
-    document.getElementById('team-image-2').setAttribute('src', './../../../assets/images/about-us-developers/arvind-silly.jpg');
-  }
-
-  team_2_regular() {
-    document.getElementById('team-image-2').setAttribute('src', './../../../assets/images/about-us-developers/arvind.jpg');
-  }
-
-  team_3_silly() {
-    document.getElementById('team-image-3').setAttribute('src', './../../../assets/images/about-us-developers/eric-silly.png');
-  }
-
-  team_3_regular() {
-    document.getElementById('team-image-3').setAttribute('src', './../../../assets/images/about-us-developers/eric.jpg');
-  }
-
-  team_4_silly() {
-    document.getElementById('team-image-4').setAttribute('src', './../../../assets/images/about-us-developers/tommy-silly.jpg');
-  }
-
-  team_4_regular() {
-    document.getElementById('team-image-4').setAttribute('src', './../../../assets/images/about-us-developers/tommy.jpg');
+  changePhotoRegular(personNumber) {
+    document.getElementById('team-image-' + personNumber.toString()).setAttribute('src', this.imagePaths[personNumber * 2]);
   }
 
   ngOnInit(): void { }
-
 }
+
+// team_1_silly() {
+//   console.log('Dabeer enter');
+
+//   document.getElementById('team-image-1').setAttribute('src', './../../../assets/images/about-us-developers/dabeer-silly.png');
+
+// }
+
+// team_1_regular() {
+//   console.log('Dabeer exit');
+//   document.getElementById('team-image-1').setAttribute('src', './../../../assets/images/about-us-developers/dabeer.jpg');
+
+// }
+
+// team_2_silly() {
+//   document.getElementById('team-image-2').setAttribute('src', './../../../assets/images/about-us-developers/arvind-silly.jpg');
+// }
+
+// team_2_regular() {
+//   document.getElementById('team-image-2').setAttribute('src', './../../../assets/images/about-us-developers/arvind.jpg');
+// }
+
+// team_3_silly() {
+//   document.getElementById('team-image-3').setAttribute('src', './../../../assets/images/about-us-developers/eric-silly.png');
+// }
+
+// team_3_regular() {
+//   document.getElementById('team-image-3').setAttribute('src', './../../../assets/images/about-us-developers/eric.jpg');
+// }
+
+// team_4_silly() {
+//   document.getElementById('team-image-4').setAttribute('src', './../../../assets/images/about-us-developers/tommy-silly.jpg');
+// }
+
+// team_4_regular() {
+//   document.getElementById('team-image-4').setAttribute('src', './../../../assets/images/about-us-developers/tommy.jpg');
+// }
