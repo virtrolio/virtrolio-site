9cf5ee18c16f66f89280ef161b49f02725735fd7
Eric C <39015447+ricetech@users.noreply.github.com>
2020-06-14 18:11:08 -0400

    Create get/send message methods

--- src/app/core/msg-io.service.ts
+++ src/app/core/msg-io.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Message } from '../shared/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MsgIoService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  getMessages(uid: string) {
+    return [];
+  }
+
+  sendMessage(message: Message) {
+    return;
+  }
 }
