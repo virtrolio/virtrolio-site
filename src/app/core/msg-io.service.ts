2be695575516212f03f290166c5da04cbfd24e8a
Eric C <39015447+ricetech@users.noreply.github.com>
2020-06-15 17:42:48 -0400

    Create MessageIoService.sendMessage()

--- src/app/core/msg-io.service.ts
+++ src/app/core/msg-io.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { VirtrolioMessage } from '../shared/interfaces';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { VirtrolioMessage, VirtrolioMessageTemplate } from '../shared/interfaces';
+
+import * as firebase from 'firebase';
+import Timestamp = firebase.firestore.Timestamp;
 
 @Injectable({
   providedIn: 'root'
 })
 export class MsgIoService {
   private currentYear = 2020;
+  private maxMessageLength = 5000;
 
-  constructor(private afs: AngularFirestore) { }
+  private messagesCollection: AngularFirestoreCollection;
+
+  constructor(private afs: AngularFirestore) {
+    this.messagesCollection = afs.collection('messages');
+    // TODO: Remove Test Message
+    // const testMessage = {
+    //   backColor: 'red',
+    //   contents: 'hi',
+    //   fontColor: 'white',
+    //   fontFamily: 'Open Sans',
+    //   from: 'BFlbQzLpG4fXxy0AVSyb',
+    //   to: 's7OIMu1mrxZscVX4dx2K5d2XYvf2'
+    // };
+    // this.sendMessage(testMessage, '');
+  }
 
   createBlankMessage() {
-    const emptyMessage: VirtrolioMessage = {
+    const emptyMessage: VirtrolioMessageTemplate = {
       backColor: '',
       contents: '',
       fontColor: '',
       fontFamily: '',
       from: '',
-      to: '',
-      isRead: false,
-      year: this.currentYear,
+      to: ''
     };
     return emptyMessage;
   }
@@ -29,7 +44,29 @@ export class MsgIoService {
     return [];
   }
 
-  sendMessage(message: VirtrolioMessage) {
-    return;
+  sendMessage(messageTemplate: VirtrolioMessageTemplate, key: string) {
+    // TODO: Add check for sender & recipient exists
+    // TODO: Add key check
+    // TODO: Add Font Family check
+    // TODO: Add Color check
+    if (messageTemplate.from == null || messageTemplate.to == null) {
+      throw new ReferenceError('User is undefined or null');
+    } else if (messageTemplate.contents.length > this.maxMessageLength) {
+      throw new RangeError('Message is too long');
+    } else if (messageTemplate.contents.length === 0) {
+      throw new RangeError('Cannot send empty message');
+    } else if (messageTemplate.backColor.length === 0 || messageTemplate.fontColor.length === 0 ||
+      messageTemplate.fontFamily.length === 0) {
+      throw new Error('A required formatting value is blank');
+    }
+
+    const message: VirtrolioMessage = {
+      ...messageTemplate,
+      timestamp: Timestamp.now(),
+      isRead: false,
+      year: this.currentYear
+    };
+
+    return this.messagesCollection.add(message).then(() => true);
   }
 }
--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,3 +1,6 @@
+import * as firebase from 'firebase';
+import Timestamp = firebase.firestore.Timestamp;
+
 export interface VirtrolioMessage {
   backColor: string;
   contents: string;
@@ -7,6 +10,16 @@ export interface VirtrolioMessage {
   to: string;
   isRead: boolean;
   year: number;
+  timestamp: Timestamp;
+}
+
+export interface VirtrolioMessageTemplate {
+  backColor: string;
+  contents: string;
+  fontColor: string;
+  fontFamily: string;
+  from: string;
+  to: string;
 }
 
 export interface VirtrolioUser {
