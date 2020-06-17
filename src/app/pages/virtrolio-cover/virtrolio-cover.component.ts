14f6b47a11fd4e4235d65b087534a457ef29818e
Tommy Huang <tommyhuang999@gmail.com>
2020-06-16 20:09:28 -0400

    Import link gen services

--- src/app/pages/virtrolio-cover/virtrolio-cover.component.ts
+++ src/app/pages/virtrolio-cover/virtrolio-cover.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AppAuthService } from '../../core/app-auth.service';
+import { LinkGenService } from '../../core/link-gen.service';
 
 @Component({
   selector: 'app-virtrolio-cover',
   templateUrl: './virtrolio-cover.component.html',
-  styleUrls: [ './virtrolio-cover.component.css' ]
+  styleUrls: ['./virtrolio-cover.component.css']
 })
 export class VirtrolioCoverComponent implements OnInit {
 
-  constructor(public authService: AppAuthService) { }
+  constructor(public authService: AppAuthService, private lgs: LinkGenService) { }
 
   ngOnInit(): void { }
 
@@ -17,6 +18,13 @@ export class VirtrolioCoverComponent implements OnInit {
     inputElement.setSelectionRange(0, 10000);  /* For mobile devices */
     document.execCommand('copy');              /* Copy text */
     buttonElement.innerHTML = 'Copied!';       /* Confirm the copy */
+  }
+
+  // ITS LINK GEN TIME
+  bar() {
+    this.lgs.getLink().then(
+      link => console.log(link)
+    );
   }
 
 }
