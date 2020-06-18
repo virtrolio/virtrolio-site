088fb14fe497407a48f0058373d0494bd01f7a88
Eric C <39015447+ricetech@users.noreply.github.com>
2020-06-17 22:42:51 -0400

    Add example font

--- src/app/core/font.service.ts
+++ src/app/core/font.service.ts
@@ -6,7 +6,11 @@ import { Font, Fonts } from '../shared/interfaces';
 })
 export class FontService {
 
+  // Note how you don't have to override the default values if you don't want to
+  private static readonly Arial = new Font('Arial', 'sans-serif', 'Arial');
+
   static readonly fonts: Fonts = {
+    Arial: FontService.Arial
   };
 
   constructor() { }
