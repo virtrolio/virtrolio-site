c98ef8bbaca1d488c3cba99976125ff7cb9c610d
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-06-19 12:39:14 -0400

    Remove unnecessary routing code in friend-link

--- src/app/pages/friend-link/friend-link.component.ts
+++ src/app/pages/friend-link/friend-link.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
-import { AuthService } from '../../core/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-friend-link',
@@ -10,29 +6,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./friend-link.component.css']
 })
 export class FriendLinkComponent implements OnInit {
-  uid: string;
-  key: string;
 
-  constructor(public authService: AuthService, private route: ActivatedRoute, private router: Router) {
-    // Get uid and key params from URL
-    this.route.queryParams.subscribe(params => {
-      this.uid = params['uid'];
-      this.key = params['key'];
-    });
-
-    // Check key and redirect if key is false or invalid
-    this.authService.checkKey(this.uid, this.key).then(validKey => {
-      if (validKey === false) {
-        this.router.navigate(['/invalid-link']);
-      }
-      if (this.authService.isLoggedIn()) {
-        this.router.navigate(['/signing']);
-      }
-    })
-    .catch(error => {
-      this.router.navigate(['/invalid-link']);
-    });
-  }
+  constructor() { }
 
   ngOnInit(): void { }
 
