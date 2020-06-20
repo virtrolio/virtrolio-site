15fdd71f743f86ca39f3fe81b8050a070a9097cc
Janakitti <janakittiratana@gmail.com>
2020-06-20 19:17:45 -0400

    Fix profile pic and display name

--- src/app/pages/viewing/responses-list/responses-list.component.ts
+++ src/app/pages/viewing/responses-list/responses-list.component.ts
@@ -10,12 +10,28 @@ import { AuthService } from '../../../core/auth.service';
 })
 export class ResponsesListComponent implements OnInit {
   public displayName;
+  public photoUrl;
+  public uid;
 
-  constructor(public viewService: ViewingService, private vps: ViewportScroller, public authService: AuthService) {}
-
-  ngOnInit(): void {
-    this.authService.displayName().then((displayName) => {this.displayName = displayName; });
+  constructor(public viewService: ViewingService, private vps: ViewportScroller, public authService: AuthService) {
+    try {
+      this.uid = this.authService.uid();
+      console.log(this.uid);
+    } catch (e) { }
+    try {
+      this.authService.displayName(this.uid).then((displayName) => {this.displayName = displayName; });
+    } catch (e) {
+      this.displayName = 'User';
+    }
+    try {
+      this.authService.profilePictureLink(this.uid).then((photoUrl) => {this.photoUrl = photoUrl; });
+    } catch (e) {
+      // Use the logo as a fallback profile picture
+      this.photoUrl = '../../../../assets/images/logo_reg.png';
+    }
   }
+
+  ngOnInit(): void { }
 
   /**
    * Scroll to the card with the given id
