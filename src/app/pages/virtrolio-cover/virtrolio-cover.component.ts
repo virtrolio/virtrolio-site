7a40778a024e010253cae7f4fe94a2d80f9eea3e
Tommy Huang <tommyhuang999@gmail.com>
2020-06-20 10:21:17 -0400

    Fix copy button to reset to "copy" after saying "copied"

--- src/app/pages/virtrolio-cover/virtrolio-cover.component.ts
+++ src/app/pages/virtrolio-cover/virtrolio-cover.component.ts
@@ -7,22 +7,30 @@ import { AuthService } from '../../core/auth.service';
   styleUrls: ['./virtrolio-cover.component.css']
 })
 export class VirtrolioCoverComponent implements OnInit {
+  /** Default values for variables */
   public link = 'Getting your link...';
   public linkReady = false;
   public warningText = false;
+  public copyButtonText = 'copy';
+
   constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
-  copyLink(inputElement, buttonElement) {
+  /**
+   * Copy link highlights an inputElement and copies its value to clipboard, updating the button to confirm the copy
+   * @param inputElement - the element to read from and copy its contents
+   */
+  copyLink(inputElement: HTMLInputElement) {
     inputElement.select();
     inputElement.setSelectionRange(0, 10000);
     document.execCommand('copy');
-    buttonElement.innerHTML = 'Copied!';
+    this.copyButtonText = 'copied!';
   }
 
   setLink() {
+    this.copyButtonText = 'copy';
     this.authService.getLink().then(
       link => this.link = link
     ).catch(error => alert('No key or userid was found! If this problem persists, please contact us at virtrolio.team@gmail.com'));
@@ -33,6 +41,7 @@ export class VirtrolioCoverComponent implements OnInit {
     if (this.warningText) {
       this.link = 'Generating new link...';
       this.linkReady = false;
+      this.copyButtonText = 'copy';
       this.authService.changeKey().catch(error => alert('An invalid key was generated! If this problem persists, please contact us at virtrolio.team@gmail.com'));
       this.setLink();
     }
