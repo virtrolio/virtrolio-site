5adfced8f6b82cebcf3fe318b390c113357edbe8
Janakitti <janakittiratana@gmail.com>
2020-06-20 20:44:28 -0400

    Add no messages display

--- src/app/pages/viewing/viewing.service.ts
+++ src/app/pages/viewing/viewing.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { MsgIoService } from '../../core/msg-io.service';
 import { VirtrolioMessage } from '../../shared/interfaces';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ViewingService {
   messages: VirtrolioMessage[];
+  numMessages: number;
   isCarouselView = false;
-  constructor(private msgIo: MsgIoService) {
+  constructor(public msgIo: MsgIoService) {
     // Subscribe to the array of VirtrolioMessage observables and make data available in messages variable
-    this.msgIo.getMessages().subscribe((msgData: VirtrolioMessage[]) => this.messages = msgData);
+    this.msgIo.getMessages().subscribe((messages: VirtrolioMessage[]) => {
+      this.messages = messages;
+      this.numMessages = messages.length;
+    });
   }
 }
