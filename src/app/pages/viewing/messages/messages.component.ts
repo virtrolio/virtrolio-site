8a13efd496dda3724934af1e8d95f88dee3c5240
Janakitti <janakittiratana@gmail.com>
2020-06-21 17:48:07 -0400

    Implement delete message

--- src/app/pages/viewing/messages/messages.component.ts
+++ src/app/pages/viewing/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ViewingService } from '../viewing.service';
 import { AuthService } from '../../../core/auth.service';
 import { FontService } from '../../../core/font.service';
@@ -12,12 +12,30 @@ import { Fonts } from '../../../shared/interfaces';
 
 export class MessagesComponent implements OnInit {
   fonts: Fonts;
+  messageToDelete: string;
+
   constructor(public viewService: ViewingService, public authService: AuthService, public fontService: FontService) { }
 
   ngOnInit(): void {
     this.fonts = FontService.fonts;
   }
 
+  /**
+   * When you click 'x' on a message, messageToDelete will be assigned the value of that message's id
+   * @param mID message id
+   */
+  setMessageToDelete(mID: string) {
+    this.messageToDelete = mID;
+  }
+
+  /**
+   * Wrapper around deleteMessage()
+   */
+  deleteMessage() {
+    try {
+      this.viewService.msgIo.deleteMessage(this.messageToDelete);
+    } catch (e) { }
+  }
   /**
    * Swap between message viewing styles styles
    */
