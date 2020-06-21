2c5e2494f9e1e3646f4df353da54e889860b2116
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-06-21 07:39:32 -0400

    Convert about-page to use google photos images

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutPagePictures } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-about',
@@ -10,45 +11,26 @@ import { Component, OnInit } from '@angular/core';
  * About component, with a short blurb at the top and a row for each contributor.
  */
 export class AboutComponent implements OnInit {
+  Arvind: AboutPagePictures;
+  Dabeer: AboutPagePictures;
+  Eric: AboutPagePictures;
+  Janakitti: AboutPagePictures;
+  John: AboutPagePictures;
+  Tommy: AboutPagePictures;
 
-  private imagePaths = [
-    'arvind.jpg',
-    'arvind-silly.jpg',
-    'dabeer.jpg',
-    'dabeer-silly.png',
-    'eric.jpg',
-    'eric-silly.png',
-    'tommy.jpg',
-    'tommy-silly.jpg',
-    'janakitti.png',
-    'janakitti-silly.png',
-    'john.jpg',
-    'john-silly.jpg',
-  ];
-
-  constructor() { }
+  /**
+   * Image links taken from Google Photos -> @link {https://www.labnol.org/embed/google/photos/} embed code generator
+   * -> bit.ly link shortener
+   */
+  constructor() {
+    this.Arvind = new AboutPagePictures('Arvind', 'https://bit.ly/3de5LUs', 'https://bit.ly/2V596Pn');
+    this.Dabeer = new AboutPagePictures('Dabeer', 'https://bit.ly/2zPaOwQ', 'https://bit.ly/2YVsSOn');
+    this.Eric = new AboutPagePictures('Eric', 'https://bit.ly/2NemLiC', 'https://bit.ly/3dk383t');
+    this.Janakitti = new AboutPagePictures('Janakitti', 'https://bit.ly/3elvYBR', 'https://bit.ly/3hOLPuD');
+    this.John = new AboutPagePictures('John', 'https://bit.ly/312Bllu', 'https://bit.ly/2AUtyvt');
+    this.Tommy = new AboutPagePictures('Tommy', 'https://bit.ly/3hQOJzo', 'https://bit.ly/3emQ1jf');
+  }
 
   ngOnInit(): void { }
-
-  /**
-   * Replace regular photo with silly version.
-   * @param personNumber - id number of person (related to id's used in the HTML)
-   */
-  changePhotoSilly(personNumber: number) {
-    console.log(this.imagePaths[personNumber * 2 + 1]);
-    document.getElementById('team-image-' + personNumber.toString())
-      .setAttribute('src', './../../../assets/images/about-us-developers/' +
-        this.imagePaths[personNumber * 2 + 1]);
-  }
-
-  /**
-   * Replace regular photo with silly version.
-   * @param personNumber - id number of person (related to id's used in the HTML)
-   */
-  changePhotoRegular(personNumber: number) {
-    document.getElementById('team-image-' + personNumber.toString())
-      .setAttribute('src', './../../../assets/images/about-us-developers/' +
-        this.imagePaths[personNumber * 2]);
-  }
 
 }
