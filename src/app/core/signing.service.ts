b465145a6bdf037fd50b071da2fa5927d6a70488
Janakitti <janakittiratana@gmail.com>
2020-06-22 20:26:30 -0400

    Move signing functionality into Signing Service

---
+++ src/app/core/signing.service.ts
@@ -0,0 +1,118 @@
+import { Injectable } from '@angular/core';
+import { Fonts } from '../shared/interfaces';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { MsgIoService } from './msg-io.service';
+import { FontService } from './font.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SigningService {
+  public signingBoxText;
+  public backgroundColor;
+  public textColor;
+  public canSend;
+  public charCount;
+  public maxCharCount: number;
+  public charCountColor;
+  public name;
+
+  private uid: string;
+  private key: string;
+
+  // font service stuff
+  public currentFont; // Used to select a font from fontDict
+  public currentFontFamily; // Used to CSS select the font
+  public currentFontDisplay; // Shown in the Font Dropdown menu
+  public fontDict: Fonts;
+
+  constructor(private route: ActivatedRoute, private authService: AuthService, private msgIo: MsgIoService,
+              private router: Router) {
+    this.fontDict = FontService.fonts;
+    this.maxCharCount = MsgIoService.maxMessageLength;
+  }
+
+  /**
+   * Sets all class variables to their default values
+   */
+  resetDefaultValues() {
+    this.signingBoxText = '';
+    this.backgroundColor = '#ffffff';
+    this.textColor = '#000000';
+    this.canSend = false;
+    this.charCount = 0;
+    this.charCountColor = '#bbbbbb';
+    this.name = '[friend name]';
+    this.currentFont = 'Arial'; // Used to select a font from fontDict
+    this.currentFontFamily = 'Arial, sans-serif'; // Used to CSS select the font
+    this.currentFontDisplay = 'Arial'; // Shown in the Font Dropdown menu
+  }
+
+  setUIDKey(uid: string, key: string) {
+    this.uid = uid;
+    this.key = key;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  /**
+   * @param font - Font selected from dropdown menu
+   */
+  selectFont(font: string) {
+    this.currentFont = font;
+    this.currentFontFamily = font + ',' + this.fontDict[font].backupFont;
+    this.currentFontDisplay = this.fontDict[font].fontFamily;
+  }
+
+  /**
+   * The following functions add text formatting characters around selected text within textbox.
+   * @param textbox - textbox in which user types.
+   * @param formatCharacters - formatting character(s) to be placed around the selected text
+   * @param endCharacter - Optional - The ending character, if different from the starting character
+   */
+  addFormatting(textbox: HTMLTextAreaElement, formatCharacters: string, endCharacter?: string) {
+    const start = textbox.selectionStart;
+    const end = textbox.selectionEnd;
+    const text = textbox.value;
+
+    // If the end character was not provided, we assume it will be the same as the start character
+    if (typeof endCharacter === 'undefined' || !endCharacter) {
+      endCharacter = formatCharacters;
+    }
+
+    this.signingBoxText = text.slice(0, start) + formatCharacters + text.slice(start, end) + endCharacter +
+      text.slice(end);
+    textbox.select();
+  }
+
+  /**
+   * Update character count value and text colour
+   * @param textbox - textbox in which user types.
+   */
+  updateCount(textbox: HTMLTextAreaElement) {
+    this.charCount = textbox.value.length;
+    this.charCountColor = (this.charCount > this.maxCharCount) ? '#EE1111' : '#b0b0b0';
+    this.canSend = (0 < this.charCount && this.charCount <= this.maxCharCount);
+  }
+
+  /**
+   * Creates a new blank message and fills in all the info before sending it
+   * @param textbox - the textbox where the contents of the message are retrieved from (not the preview box)
+   */
+  createMsg(textbox: HTMLTextAreaElement) {
+    const newMsg = this.msgIo.createBlankMessage();
+    newMsg.backColor = this.backgroundColor;
+    newMsg.fontColor = this.textColor;
+    newMsg.fontFamily = this.currentFont;
+    newMsg.contents = textbox.value;
+    newMsg.to = this.uid;
+
+    this.msgIo.sendMessage(newMsg, this.key).then(() =>
+      this.router.navigate([ '/msg-sent' ]))
+      .catch(error => alert(error));
+  }
+
+}
--- src/app/pages/signing/signing.component.ts
+++ src/app/pages/signing/signing.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MsgIoService } from '../../core/msg-io.service';
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/core/auth.service';
-import { FontService } from 'src/app/core/font.service';
-import { Fonts } from '../../shared/interfaces';
+import { SigningService } from '../../core/signing.service';
 
 @Component({
   selector: 'app-signing',
@@ -16,28 +14,8 @@ import { Fonts } from '../../shared/interfaces';
  * click on the 'Send' button.
  */
 export class SigningComponent implements OnInit {
-  /** Default values */
-  public signingBoxText = '';
-  public backgroundColor = '#ffffff';
-  public textColor = '#000000';
-  public canSend = false;
-  public charCount = 0;
-  public maxCharCount: number;
-  public charCountColor = '#bbbbbb';
-  public name = '[friend name]';
-
   private uid: string;
-  private key: string;
-
-  // font service stuff
-  public currentFont = 'Arial'; // Used to select a font from fontDict
-  public currentFontFamily = 'Arial, sans-serif'; // Used to CSS select the font
-  public currentFontDisplay = 'Arial'; // Shown in the Font Dropdown menu
-  public fontDict: Fonts;
-
-  constructor(private route: ActivatedRoute, private authService: AuthService, private msgIo: MsgIoService,
-              private router: Router) {
-    this.fontDict = FontService.fonts;
+  constructor(private route: ActivatedRoute, private authService: AuthService, public signService: SigningService) {
   }
 
   /**
@@ -46,67 +24,10 @@ export class SigningComponent implements OnInit {
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.uid = params.uid;
-      this.key = params.key;
+      this.signService.setUIDKey(this.uid, params.key);
     });
-    this.maxCharCount = MsgIoService.maxMessageLength;
-    this.authService.displayName(this.uid).then(userName => this.name = userName).catch(error => alert(error));
+    this.authService.displayName(this.uid).then(userName => this.signService.setName(userName))
+        .catch(error => alert(error));
+    this.signService.resetDefaultValues();
   }
-
-  /**
-   * @param font - Font selected from dropdown menu
-   */
-  selectFont(font: string) {
-    this.currentFont = font;
-    this.currentFontFamily = font + ',' + this.fontDict[font].backupFont;
-    this.currentFontDisplay = this.fontDict[font].fontFamily;
-  }
-
-  /**
-   * The following functions add text formatting characters around selected text within textbox.
-   * @param textbox - textbox in which user types.
-   * @param formatCharacters - formatting character(s) to be placed around the selected text
-   * @param endCharacter - Optional - The ending character, if different from the starting character
-   */
-  addFormatting(textbox: HTMLTextAreaElement, formatCharacters: string, endCharacter?: string) {
-    const start = textbox.selectionStart;
-    const end = textbox.selectionEnd;
-    const text = textbox.value;
-
-    // If the end character was not provided, we assume it will be the same as the start character
-    if (typeof endCharacter === 'undefined' || !endCharacter) {
-      endCharacter = formatCharacters;
-    }
-
-    this.signingBoxText = text.slice(0, start) + formatCharacters + text.slice(start, end) + endCharacter +
-      text.slice(end);
-    textbox.select();
-  }
-
-  /**
-   * Update character count value and text colour
-   * @param textbox - textbox in which user types.
-   */
-  updateCount(textbox: HTMLTextAreaElement) {
-    this.charCount = textbox.value.length;
-    this.charCountColor = (this.charCount > this.maxCharCount) ? '#EE1111' : '#b0b0b0';
-    this.canSend = (0 < this.charCount && this.charCount <= this.maxCharCount);
-  }
-
-  /**
-   * Creates a new blank message and fills in all the info before sending it
-   * @param textbox - the textbox where the contents of the message are retrieved from (not the preview box)
-   */
-  createMsg(textbox: HTMLTextAreaElement) {
-    const newMsg = this.msgIo.createBlankMessage();
-    newMsg.backColor = this.backgroundColor;
-    newMsg.fontColor = this.textColor;
-    newMsg.fontFamily = this.currentFont;
-    newMsg.contents = textbox.value;
-    newMsg.to = this.uid;
-
-    this.msgIo.sendMessage(newMsg, this.key).then(() =>
-      this.router.navigate([ '/msg-sent' ]))
-      .catch(error => alert(error));
-  }
-
 }
