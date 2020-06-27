d29f55078ea0e54574681db03719a096e44de572
Janakitti <janakittiratana@gmail.com>
2020-06-27 15:07:04 -0400

    Implement dynamic header colors

--- src/app/pages/viewing/messages/messages.component.ts
+++ src/app/pages/viewing/messages/messages.component.ts
@@ -23,6 +23,37 @@ export class MessagesComponent implements OnInit {
   }
 
   /**
+   * Generate the lightness value of HSL from RBG
+   */
+  getLightness(r, g, b) {
+    r /= 255, g /= 255, b /= 255;
+    const max = Math.max(r, g, b), min = Math.min(r, g, b);
+    const l = (max + min) / 2;
+    return l;
+  }
+
+  /**
+   * Generate a darkened version of backColor, returning this new header color and the appropriate header text color
+   * @param backColor: background color of the message
+   */
+  generateHeaderColor(backColor: string) {
+    const bgR = parseInt(backColor.slice(1, 3), 16);
+    const bgG = parseInt(backColor.slice(3, 5), 16);
+    const bgB = parseInt(backColor.slice(5), 16);
+    const hR = bgR + 20 > 255 ? 255 : bgR + 20;
+    const hG = bgG + 20 > 255 ? 255 : bgG + 20;
+    const hB = bgB + 20 > 255 ? 255 : bgB + 20;
+    const headerColor = hR.toString(16) + hG.toString(16) + hB.toString(16);
+    let headerTextColor;
+    if (this.getLightness(hR, hG, hB) > 0.65) {
+      headerTextColor = '#000000';
+    } else {
+      headerTextColor = '#FFFFFF';
+    }
+    return { bg: '#' + headerColor, text: headerTextColor };
+  }
+
+  /**
    * When you click 'x' on a message, messageToDelete will be assigned the value of that message's id
    * @param mID message id
    */
