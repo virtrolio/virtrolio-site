de62c11cab2792d860ebeaf2cdde1118a950680c
Eric C <39015447+ricetech@users.noreply.github.com>
2020-06-30 18:28:33 -0400

    Create generateDownloads()

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/auth.service';
+import { MsgIoService } from '../../core/msg-io.service';
+import { take } from 'rxjs/operators';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-settings',
@@ -7,8 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private msgIoService: MsgIoService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void { }
+
+  /**
+   * Converts the user's data and messages into JSONs and changes the href of their respective buttons to permit
+   * downloading of the data.
+   */
+  async generateDownloads() {
+    const messages = this.msgIoService.getMessages().pipe(take(1)).toPromise();
+    const userData = await this.authService.getUserData();
+    const messagesJSON = JSON.stringify(messages);
+    const userJSON = JSON.stringify(userData);
+    this.sanitizer.bypassSecurityTrustUrl('data:text/json;charset=UTF-8,' + encodeURIComponent(messagesJSON));
+    this.sanitizer.bypassSecurityTrustUrl('data:text/json;charset=UTF-8,' + encodeURIComponent(userJSON));
+  }
 
 }
