d0b7c95cd4283a62af0dd3fe1f7ecd333b5fa61f
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2020-06-30 09:11:27 -0400

    Revert signing service addFormatting

--- src/app/core/signing.service.ts
+++ src/app/core/signing.service.ts
@@ -68,7 +68,6 @@ export class SigningService {
 
     this.signingBoxText = text.slice(0, start) + formatCharacters + text.slice(start, end) + endCharacter +
       text.slice(end);
-    this.updateCount(textbox);
     textbox.select();
   }
 
