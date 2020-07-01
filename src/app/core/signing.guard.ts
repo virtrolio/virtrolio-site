f0f5abb57f07de2bf27536aab7db4adf877493bd
Tommy Huang <tommyhuang999@gmail.com>
2020-07-01 13:53:07 -0400

    Fix signing guard to properly redirect to maintenance component

--- src/app/core/signing.guard.ts
+++ src/app/core/signing.guard.ts
@@ -20,6 +20,7 @@ import { MsgIoService } from './msg-io.service';
 export class SigningGuard implements CanActivate {
   static uid = 'invalid';
   static key = 'invalid';
+  private readonly maintenance = false;
 
   constructor(private authService: AuthService, private route: ActivatedRoute, private router: Router,
               private msgIOService: MsgIoService) { }
@@ -29,55 +30,59 @@ export class SigningGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    // Regex Extraction of 'uid' and 'key' params
-    try {
-      const linkStr = window.location.href;
-      SigningGuard.uid = linkStr.match(/uid=([^&]*)/)[1];
-      SigningGuard.key = linkStr.match(/key=([^&]*)/)[1];
-    } catch (error) {
-      // Displaying alert is not necessary since an error is only thrown when one of the query params is missing/invalid = invalid link
-      this.router.navigate([ '/invalid-link' ]).catch(error => AuthService.displayError(error));
-    }
+    if (this.maintenance) {
+      this.router.navigate([ '/maintenance' ]);
+    } else {
+      // Regex Extraction of 'uid' and 'key' params
+      try {
+        const linkStr = window.location.href;
+        SigningGuard.uid = linkStr.match(/uid=([^&]*)/)[1];
+        SigningGuard.key = linkStr.match(/key=([^&]*)/)[1];
+      } catch (error) {
+        // Displaying alert is not necessary since an error is only thrown when one of the query params is missing/invalid = invalid link
+        this.router.navigate([ '/invalid-link' ]).catch(error => AuthService.displayError(error));
+      }
 
-    // User must be signed in
-    return this.authService.asyncIsLoggedIn().then(isLoggedIn => {
-      if (!isLoggedIn) {
-        this.router.navigate([ '/signing-auth-redirect' ], { queryParams: { uid: SigningGuard.uid, key: SigningGuard.key } })
-          .catch(e => AuthService.displayError(e));
-        return false;
-      } else {
-        // Sender must not have previously signed the recipient's virtrolio
-        return this.authService.checkKey(SigningGuard.uid, SigningGuard.key).then(validKey => {
-          if (validKey === false) {
-            this.router.navigate([ '/invalid-link' ]).catch(error => AuthService.displayError(error));
-            return false;
-          }
-          this.msgIOService.checkForMessage(SigningGuard.uid).then((signed) => {
-            if (signed) {
-              this.router.navigate([ '/rejecc' ]).catch(error => AuthService.displayError(error));
+      // User must be signed in
+      return this.authService.asyncIsLoggedIn().then(isLoggedIn => {
+        if (!isLoggedIn) {
+          this.router.navigate([ '/signing-auth-redirect' ], { queryParams: { uid: SigningGuard.uid, key: SigningGuard.key } })
+            .catch(e => AuthService.displayError(e));
+          return false;
+        } else {
+          // Sender must not have previously signed the recipient's virtrolio
+          return this.authService.checkKey(SigningGuard.uid, SigningGuard.key).then(validKey => {
+            if (validKey === false) {
+              this.router.navigate([ '/invalid-link' ]).catch(error => AuthService.displayError(error));
               return false;
-            } else {
-              return true;
             }
+            this.msgIOService.checkForMessage(SigningGuard.uid).then((signed) => {
+              if (signed) {
+                this.router.navigate([ '/rejecc' ]).catch(error => AuthService.displayError(error));
+                return false;
+              } else {
+                return true;
+              }
+            }).catch(error => {
+              // Only possibilities for a Firebase error getting thrown:
+              // 1. Not logged in
+              // 2. Requested message was NOT sent by current user (impossible based on logic of checkMessage() unless someone
+              // modifies the source code)
+              console.log(error);
+              AuthService.displayError(error);
+              this.router.navigate([ '/signing-auth-redirect' ], { queryParams: { uid: SigningGuard.uid, key: SigningGuard.key } })
+                .catch(navError => AuthService.displayError(navError));
+            });
+            return true;
           }).catch(error => {
-            // Only possibilities for a Firebase error getting thrown:
-            // 1. Not logged in
-            // 2. Requested message was NOT sent by current user (impossible based on logic of checkMessage() unless someone
-            // modifies the source code)
+            // noinspection JSIgnoredPromiseFromCall
             console.log(error);
             AuthService.displayError(error);
-            this.router.navigate([ '/signing-auth-redirect' ], { queryParams: { uid: SigningGuard.uid, key: SigningGuard.key } })
-              .catch(navError => AuthService.displayError(navError));
+            this.router.navigate([ '/invalid-link' ]).catch(e => AuthService.displayError(e));
+            return false;
           });
-          return true;
-        }).catch(error => {
-          // noinspection JSIgnoredPromiseFromCall
-          console.log(error);
-          AuthService.displayError(error);
-          this.router.navigate([ '/invalid-link' ]).catch(e => AuthService.displayError(e));
-          return false;
-        });
-      }
-    });
+        }
+      });
+    }
   }
 }
