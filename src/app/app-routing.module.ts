1923ac70777a4b92c2d74b8af37131bf4825ab70
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-07-01 07:32:48 -0400

    Add guard to prevent users from visiting certain pages by typing in the url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,10 +20,11 @@ import { TermsOfServiceComponent } from './pages/terms-of-service/terms-of-servi
 import { ViewingComponent } from './pages/viewing/viewing.component';
 import { YourVirtrolioComponent } from './pages/your-virtrolio/your-virtrolio.component';
 
-// Services
+// Services & Guards
 import { LoginResolver } from './core/login-resolver';
 import { RejeccComponent } from './pages/rejecc/rejecc.component';
 import { SigningGuard } from './core/signing.guard';
+import { PreventURLAccessGuard } from './core/prevent-urlaccess.guard';
 
 const redirectUnauthorized = () => redirectUnauthorizedTo([ '/access-denied' ]);
 
@@ -36,16 +37,27 @@ const routes: Routes = [
   { path: 'access-denied', component: AccessDeniedComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'faq', component: FaqComponent },
-  { path: 'signing-auth-redirect', component: SigningAuthRedirectComponent},
-  { path: 'invalid-link' , component: InvalidLinkComponent },
+  {
+    path: 'signing-auth-redirect',
+    component: SigningAuthRedirectComponent,
+    canActivate: [ PreventURLAccessGuard ]
+  },
+  {
+    path: 'invalid-link',
+    component: InvalidLinkComponent,
+    canActivate: [ PreventURLAccessGuard ]
+  },
   {
     path: 'msg-sent',
     component: MsgSentComponent,
-    canActivate: [ AngularFireAuthGuard ],
-    data: { authGuardPipe: redirectUnauthorized }
+    canActivate: [ PreventURLAccessGuard ],
   },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
-  { path: 'rejecc', component: RejeccComponent },
+  {
+    path: 'rejecc',
+    component: RejeccComponent,
+    canActivate: [ PreventURLAccessGuard ]
+  },
   {
     path: 'settings',
     component: SettingsComponent,
@@ -79,7 +91,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes)], // scroll to top when routerLinking
+  imports: [ RouterModule.forRoot(routes) ], // scroll to top when routerLinking
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {
