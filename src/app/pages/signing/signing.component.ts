3a4ee33ba9fa34912a0fa27a103496150aece345
Eric C <39015447+ricetech@users.noreply.github.com>
2020-07-11 16:08:01 -0400

    Add Signing box scroll sync

--- src/app/pages/signing/signing.component.ts
+++ src/app/pages/signing/signing.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from 'src/app/core/auth.service';
 import { SigningService } from '../../core/signing.service';
 import { MsgIoService } from '../../core/msg-io.service';
 import { Title } from '@angular/platform-browser';
+import { MarkdownComponent } from 'ngx-markdown';
 
 @Component({
   selector: 'app-signing',
@@ -50,6 +51,17 @@ export class SigningComponent implements OnInit {
   @HostListener('window:beforeunload')
   canDeactivate(): Observable<boolean> | boolean {
     return !this.signingService.signingBoxText || this.sending;
+  }
+
+  /**
+   * Syncs the scroll bar positions of the textbox and preview box
+   * @param textbox - the box that the user can type into
+   * @param previewBox - the markdown preview
+   */
+  syncMarkdown(textbox: HTMLTextAreaElement, previewBox: MarkdownComponent) {
+    const scrollPercentage: number = (textbox.scrollTop + 160) / textbox.scrollHeight;
+    const markdownElement = previewBox.element.nativeElement;
+    markdownElement.scrollTop = scrollPercentage * markdownElement.scrollHeight - 160;
   }
 
   /**
