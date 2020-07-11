367ec1236c5475010207262337adea8b46a79487
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-07-11 16:23:58 -0400

    Remove ElementRef import into signing.component.ts

--- src/app/pages/signing/signing.component.ts
+++ src/app/pages/signing/signing.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/core/auth.service';
