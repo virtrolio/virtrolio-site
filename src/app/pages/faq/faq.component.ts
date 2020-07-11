106cff3ca8bb5357fa8b45894b220cb30f37d547
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-07-11 16:49:45 -0400

    Rename faqContent.json to faq.content.json

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import faqContent from './faqContent.json';
+import faqContent from './faq.content.json';
+import { FAQSection } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-faq',
