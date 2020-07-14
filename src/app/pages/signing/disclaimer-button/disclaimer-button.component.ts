b33ccdd717b3244dce9765cd48aabaabf86dbca0
Eric C <39015447+ricetech@users.noreply.github.com>
2020-07-14 17:27:25 -0400

    Add CSS file for disclaimer-button

--- src/app/pages/signing/disclaimer-button/disclaimer-button.component.ts
+++ src/app/pages/signing/disclaimer-button/disclaimer-button.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-disclaimer-button',
-  templateUrl: './disclaimer-button.component.html'
+  templateUrl: './disclaimer-button.component.html',
+  styleUrls: ['./disclaimer-button.component.css']
 })
 export class DisclaimerButtonComponent implements OnInit {
 
