7ef00dfc4c37a8477995bb81d2f40d9b2afb650a
Eric C <39015447+ricetech@users.noreply.github.com>
2020-07-14 18:42:56 -0400

    Create access-denied-beta component

---
+++ src/app/pages/access-denied-beta/access-denied-beta.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-access-denied-beta',
+  templateUrl: './access-denied-beta.component.html',
+  styleUrls: ['./access-denied-beta.component.css']
+})
+export class AccessDeniedBetaComponent implements OnInit {
+
+  constructor(private title: Title) { }
+
+  ngOnInit(): void {
+    this.title.setTitle('Access Denied (Not a Beta Tester) | Virtrolio');
+  }
+
+}
