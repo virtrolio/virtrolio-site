f269163539f57d995931e57ab079a562ec85aab1
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-07-14 22:34:38 -0400

    Add spaces to data binding tags
    Co-authored-by: Eric C. <39015447+ricetech@users.noreply.github.com>

--- src/app/pages/signing/disclaimer-button/disclaimer-button.component.ts
+++ src/app/pages/signing/disclaimer-button/disclaimer-button.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-disclaimer-button',
   templateUrl: './disclaimer-button.component.html',
-  styleUrls: ['./disclaimer-button.component.css']
+  styleUrls: [ './disclaimer-button.component.css' ]
 })
 export class DisclaimerButtonComponent implements OnInit {
 
