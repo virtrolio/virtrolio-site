97a74d4d5ab6719c4c2358712367d1c44dbcbdd0
Janakitti <janakittiratana@gmail.com>
2020-08-09 11:26:16 -0400

    Implement dynamic formatting of titlepage based on viewport height

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 import { AuthService } from '../../core/auth.service';
 import { Title } from '@angular/platform-browser';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,21 @@ import { Title } from '@angular/platform-browser';
   styleUrls: [ './home.component.css' ]
 })
 export class HomeComponent implements OnInit {
+  screenHeight: number;
 
   constructor(public authService: AuthService, private title: Title) {
     // Initialize Animate on Scroll library
     // In constructor instead of ngOnInit to avoid the page breaking when routerLinking to home
     AOS.init();
+    this.getScreenSize();
+  }
+
+  /**
+   * "Subscrbes" to the window resize event to update current value of viewport height
+   */
+  @HostListener('window:resize', [ '$event' ])
+  getScreenSize(event?) {
+    this.screenHeight = window.innerHeight;
   }
 
   ngOnInit(): void {
