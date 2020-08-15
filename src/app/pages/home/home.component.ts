c86576b314ccecb025a1b49ab5785dd130a23aaf
Janakitti <janakittiratana@gmail.com>
2020-08-14 20:46:37 -0400

    Remove unused event paramter in getScreenSize

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit {
    * "Subscrbes" to the window resize event to update current value of viewport height
    */
   @HostListener('window:resize', [ '$event' ])
-  getScreenSize(event?) {
+  getScreenSize() {
     this.screenHeight = window.innerHeight;
   }
 
