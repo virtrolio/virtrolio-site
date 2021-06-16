af4ec622bb25ec8c33389846355ac8e1dc496a8b
Eric C <39015447+ricetech@users.noreply.github.com>
2021-06-15 21:55:55 -0400

    Update footer and my-virtrolio to show current year (#420)

--- src/app/core/common.service.ts
+++ src/app/core/common.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CommonService {
+  public static currentYear: number = new Date().getFullYear();
+
   constructor() {}
 
   /**
